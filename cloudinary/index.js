e79b707ade84896dcab6a7704c68519c5b1cf3ef
majorAndry <majorandry@yahoo.com>
2021-08-08 18:38:56 +0200

    Adds support to webp images for posts

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -1,6 +1,6 @@
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const mimeTypesWhiteList = ["image/png", "image/jpeg", "image/jpg"];
+const mimeTypesWhiteList = ["image/png", "image/jpeg", "image/jpg", "image/webp"];
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
